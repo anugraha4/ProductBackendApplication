4d07a0faee938b7f145a6f7864bee417b6705f7b
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-12-07 11:29:06 +0530

    product app

---
+++ productbackend_application/src/main/java/com/nest/productbackend_application/ProductbackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.productbackend_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductbackendApplication.class, args);
+	}
+
+}
